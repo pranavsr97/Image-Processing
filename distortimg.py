415537e667efc1cd4a99e2cc92420742fcfed5ed
Pranav S <pranavsr97@users.noreply.github.com>
2017-03-31 02:55:10 +0000

    Add files via upload
    Modified main part of files as functions

--- distortimg.py
+++ distortimg.py
@@ -2,64 +2,80 @@ import cv2
 import numpy as np
 import sys
 
-path = sys.argv[1]
-img=cv2.imread(path)
+def distortimg(imgpath,x1_coord,y1_coord,x2_coord,y2_coord,x3_coord,y3_coord,x4_coord,y4_coord):
 
-shape = img.shape
+	img=cv2.imread(imgpath)
 
-print shape
+	shape = img.shape
 
-A = [0,0]
-B = [shape[1]-1,0]
-C = [shape[1]-1,shape[0]-1]
-D = [0,shape[0]-1]
+	print shape
 
-inpts=np.float32([A,B,C,D])
+	A = [0,0]
+	B = [shape[1]-1,0]
+	C = [shape[1]-1,shape[0]-1]
+	D = [0,shape[0]-1]
 
-newA=[]
-newB=[]
-newC=[]
-newD=[]
+	inpts=np.float32([A,B,C,D])
 
-newA.append(int(sys.argv[2]))
-newA.append(int(sys.argv[3]))
-newB.append(int(sys.argv[4]))
-newB.append(int(sys.argv[5]))
-newC.append(int(sys.argv[6]))
-newC.append(int(sys.argv[7]))
-newD.append(int(sys.argv[8]))
-newD.append(int(sys.argv[9]))
+	newA=[]
+	newB=[]
+	newC=[]
+	newD=[]
 
-outpts = np.float32([newA,newB,newC,newD])
+	newA.append(x1_coord)
+	newA.append(y1_coord)
+	newB.append(x2_coord)
+	newB.append(y2_coord)
+	newC.append(x3_coord)
+	newC.append(y3_coord)
+	newD.append(x4_coord)
+	newD.append(y4_coord)
 
-lowx=min(outpts[0][0],outpts[1][0],outpts[2][0],outpts[3][0])
-lowy=min(outpts[0][1],outpts[1][1],outpts[2][1],outpts[3][1])
+	outpts = np.float32([newA,newB,newC,newD])
 
-for i in range(len(outpts)):
-	outpts[i][0]-=lowx
-	outpts[i][1]-=lowy
+	lowx=min(outpts[0][0],outpts[1][0],outpts[2][0],outpts[3][0])
+	lowy=min(outpts[0][1],outpts[1][1],outpts[2][1],outpts[3][1])
 
-'''
-outpts[0][0]-=lowx
-outpts[1][0]-=lowx
-outpts[2][0]-=lowx
-outpts[3][0]-=lowx
+	for i in range(len(outpts)):
+		outpts[i][0]-=lowx
+		outpts[i][1]-=lowy
 
-outpts[0][1]-=lowy
-outpts[1][1]-=lowy
-outpts[2][1]-=lowy
-outpts[3][1]-=lowy
-'''
+	'''
+	outpts[0][0]-=lowx
+	outpts[1][0]-=lowx
+	outpts[2][0]-=lowx
+	outpts[3][0]-=lowx
 
-highx=max(outpts[0][0],outpts[1][0],outpts[2][0],outpts[3][0])
-highy=max(outpts[0][1],outpts[1][1],outpts[2][1],outpts[3][1])
+	outpts[0][1]-=lowy
+	outpts[1][1]-=lowy
+	outpts[2][1]-=lowy
+	outpts[3][1]-=lowy
+	'''
 
-#newinpts = 
+	highx=max(outpts[0][0],outpts[1][0],outpts[2][0],outpts[3][0])
+	highy=max(outpts[0][1],outpts[1][1],outpts[2][1],outpts[3][1])
 
-M = cv2.getPerspectiveTransform(inpts,outpts)
-fimg = cv2.warpPerspective(img,M,(highx,highy))
+	#newinpts = 
 
-cv2.imshow('Image',img)
-cv2.imshow('Fimg',fimg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+	M = cv2.getPerspectiveTransform(inpts,outpts)
+	fimg = cv2.warpPerspective(img,M,(highx,highy))
+	return fimg
+
+if __name__=="__main__":
+
+	path = sys.argv[1]
+	x1_coord=int(sys.argv[2])
+	y1_coord=int(sys.argv[3])
+	x2_coord=int(sys.argv[4])
+	y2_coord=int(sys.argv[5])
+	x3_coord=int(sys.argv[6])
+	y3_coord=int(sys.argv[7])
+	x4_coord=int(sys.argv[8])
+	y4_coord=int(sys.argv[9])
+
+	fimg=distortimg(path,x1_coord,y1_coord,x2_coord,y2_coord,x3_coord,y3_coord,x4_coord,y4_coord)
+	img=cv2.imread(path)
+	cv2.imshow('Image',img)
+	cv2.imshow('Fimg',fimg)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
