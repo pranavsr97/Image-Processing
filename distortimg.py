a47f791b40eceef5af98cc8d28facce328b1461e
Pranav S <pranavsr97@users.noreply.github.com>
2017-03-31 11:51:47 +0000

    Add files via upload
    Updated code files

--- distortimg.py
+++ distortimg.py
@@ -2,26 +2,31 @@ import cv2
 import numpy as np
 import sys
 
+# Function to distort a given image, to the given co-ordinates
+# imgpath - path of given image
+# x1_coord, y1_coord ... x4_coorf, y4_coord, are the given co-ordinated to which the given image is to be distorted to
 def distortimg(imgpath,x1_coord,y1_coord,x2_coord,y2_coord,x3_coord,y3_coord,x4_coord,y4_coord):
 
 	img=cv2.imread(imgpath)
 
 	shape = img.shape
 
-	print shape
-
+	# Existing end co-ordinates of the given image
 	A = [0,0]
 	B = [shape[1]-1,0]
 	C = [shape[1]-1,shape[0]-1]
 	D = [0,shape[0]-1]
 
+	# Storing input end co-ords in a single numpy array
 	inpts=np.float32([A,B,C,D])
 
+	# Initialising the new image's end co-ordinates
 	newA=[]
 	newB=[]
 	newC=[]
 	newD=[]
 
+	# Defining the new image's co-ordinates
 	newA.append(x1_coord)
 	newA.append(y1_coord)
 	newB.append(x2_coord)
@@ -31,11 +36,14 @@ def distortimg(imgpath,x1_coord,y1_coord,x2_coord,y2_coord,x3_coord,y3_coord,x4_
 	newD.append(x4_coord)
 	newD.append(y4_coord)
 
+	# Storing output end co-ords in a single numpy array
 	outpts = np.float32([newA,newB,newC,newD])
 
+	# Finding minimum of all the x-coords and minimun of all the y-coords
 	lowx=min(outpts[0][0],outpts[1][0],outpts[2][0],outpts[3][0])
 	lowy=min(outpts[0][1],outpts[1][1],outpts[2][1],outpts[3][1])
 
+	# Bringing the points to scale of (0,0)
 	for i in range(len(outpts)):
 		outpts[i][0]-=lowx
 		outpts[i][1]-=lowy
@@ -52,17 +60,22 @@ def distortimg(imgpath,x1_coord,y1_coord,x2_coord,y2_coord,x3_coord,y3_coord,x4_
 	outpts[3][1]-=lowy
 	'''
 
+	# Finding max of x and y-coords, in new shifted scale
 	highx=max(outpts[0][0],outpts[1][0],outpts[2][0],outpts[3][0])
 	highy=max(outpts[0][1],outpts[1][1],outpts[2][1],outpts[3][1])
 
 	#newinpts = 
 
+	# Performing the transformation of image to new co-ordinates
 	M = cv2.getPerspectiveTransform(inpts,outpts)
 	fimg = cv2.warpPerspective(img,M,(highx,highy))
+
+	# Transformed image is returned
 	return fimg
 
 if __name__=="__main__":
 
+	# Image path and reqd new image's co-ords are read
 	path = sys.argv[1]
 	x1_coord=int(sys.argv[2])
 	y1_coord=int(sys.argv[3])
@@ -73,8 +86,10 @@ if __name__=="__main__":
 	x4_coord=int(sys.argv[8])
 	y4_coord=int(sys.argv[9])
 
+	# Transformed image 'fimg' is got
 	fimg=distortimg(path,x1_coord,y1_coord,x2_coord,y2_coord,x3_coord,y3_coord,x4_coord,y4_coord)
 	img=cv2.imread(path)
+
 	cv2.imshow('Image',img)
 	cv2.imshow('Fimg',fimg)
 	cv2.waitKey(0)
--- zoomimg.py
+++ zoomimg.py
@@ -2,31 +2,21 @@ import cv2
 import numpy as np
 import sys
 
-#from math import *
-
 def zoom(imgpath,x_coord,y_coord,scale):
-
-	#x_coord = 1024
-	#y_coord = 640
-	#scale=1
 
 	img=cv2.imread(imgpath)
 
+	# Defining co-ordinates to create a window around the point of interest
+	# The window is proportional to size of original image, to maintain aspect ratio
+	# Defining the top left, and bottom right co-ordinates
 	tl_x = int(x_coord - (float(1)/float(scale) * float(1)/float(2) * img.shape[1]))
 	tl_y = int(y_coord - (float(1)/float(scale) * float(1)/float(2) * img.shape[0]))
-
-	#br_x = int(x_coord + (float(1)/float(scale) * float(1)/float(2) * img.shape[1]))
-	#br_y = int(x_coord + (float(1)/float(scale) * float(1)/float(2) * img.shape[0]))
 
 	br_x = int(tl_x + (float(1)/float(scale) * img.shape[1]))
 	br_y = int(tl_y + (float(1)/float(scale) * img.shape[0]))
 
-	#print tl_x
-	#print tl_y
 
-	#print br_x
-	#print br_y
-
+	# The window is adjusted, if it happens to be going out of the scope of the original image size
 	if (tl_x < 0):
 		br_x = br_x - tl_x
 		tl_x = 0
@@ -41,204 +31,106 @@ def zoom(imgpath,x_coord,y_coord,scale):
 		tl_y = tl_y - (br_y - img.shape[0])
 		br_y = img.shape[0]
 
-	'''
-	if(tl_x < 0):
-		if(tl_y < 0):
-			br_x -= tl_x
-			br_y -= tl_y
-			tl_x=0
-			tl_y=0
-		else:
-			if(br_y <= img.shape[1]):
-				br_x -= tl_x
-				tl_x = 0
-			else:
-				br_x -= tl_x
-				tl_x = 0
-				tl_y -= (br_y - img.shape[1])
-				br_y = img.shape[1]
-	else:
-	'''
-	'''
-	if((tl_x < 0) && (tl_y < 0)):
-		br_x -= tl_x
-		br_y -= tl_y
-		tl_x=0
-		tl_y=0
-	elif ((tl_x >= 0) && (tl_x <= img.shape[0]) && (tl_y < 0) && (br_x > img.shape[0])):
-		tl_x -= (br_x-img.shape[0])
-		br_x = img.shape[0]
-		br_y -= tl_y
-		tl_y = 0
-	elif ((tl_x >= 0) && (tl_x <= img.shape[0]) && (tl_y < 0)):
-		br_y -= tl_y
-		tl_y = 0
-	elif 
-
-	if (tl_x <  0):
-		#br_x -= tl_x
-		tl_x=0
-		br_x = int((float(1)/float(scale)) * img.shape[0])
-	
-	if (tl_y < 0):
-		#br_y -= tl_y
-		tl_y=0
-		br_y = int((float(1)/float(scale)) * img.shape[1])
-
-	if (br_x > img.shape[0]):
-		#extra_x = br_x-img.shape[0]
-		#tl_x -= extra_x
-		br_x = img.shape[0]
-		tl_x -= int((float(1)/float(scale)) * img.shape[0])
-
-	if (br_y > img.shape[1]):
-		#extra_y = br_y-img.shape[1]
-		#tl_y -= extra_y
-		br_y = img.shape[1]
-		tl_y -= int((float(1)/float(scale)) * img.shape[1])
-	'''
-	#print tl_x
-	#print tl_y
-
-	#print br_x
-	#print br_y
-
+	# The window is applied over the image to get only the required image portion 
+	# roi - region of interest
 	roi=img[tl_y:br_y, tl_x:br_x,:]
-	#roi = img[50:52,50:52]
-	#print roi.shape
-	#newimg=copy.deepcopy(roi)
-	#print newimg.shape
 
-	#newimg_shape=newimg.shape
+	# The width and height of this roi is stored
 	width1=(roi.shape[1])-1
 	height1=(roi.shape[0])-1
 
-	#width2 = newimg.shape[0] + 400
-	#height2 = newimg.shape[1] + 400
-
+	# The width and height of original image is stored
 	width2 = img.shape[1]
 	height2 = img.shape[0]
 
-	#width2 = 5
-	#height2 = 5
-
+	# Width Ratio and Height Ratio are calculated
 	width_ratio=float(width1)/float(width2)
 	height_ratio=float(height1)/float(height2)
 
+	# A count variable is defined to keep track of number of elements stored in the array
 	count=0
-	new=[]
-	#new.append([])
-	#temp_height2=height2+179
-	#temp_width2=width2+179
 
+	# The new image's array is initialised
+	new=[]
+
+	# Every pixel of ther region of interest is traversed
+	# We perform bilinear interpolation for the 3 channels (B,G,R) of the image
 	for i in range((height2)):
 		for j in range((width2)):
 			x = int(width_ratio*j)
 			y = int(height_ratio*i)
 			x_diff = (width_ratio*j) - x
 			y_diff = (height_ratio*i) - y
-			#print "x: "+str(x)
-			#print "j: "+str(j)
-			#print "y: "+str(y)
-			#print "i: "+str(i)
-			#print "Width: "+str(width2)
-			#print "Height: "+str(height2)
-			#print "IMG shape: "+str(img.shape)
-			#print "ROI shape: "+str(roi.shape)
-			#[x][y]
 
+			# The neighbouring co-ordinates are compared
+			# The 'if-else' conditions are added to check for the extreme co-ordinates
 			if (x>=(width1-1) or y>=(height1-1)):
-				#print "A in"
 				A_blue = roi[y][x][0]
 				A_red = roi[y][x][1]
 				A_green = roi[y][x][2]
 			else:
-				#print "A out"
 				A_blue = roi[y][x][0]
 				A_red = roi[y][x][1]
 				A_green = roi[y][x][2]
-				#A = roi[y][x][:] & 0xff
 
 			if ((x+1)>=(width1-1) or (y>=(height1-1))):
-				#print "B in"
 				B_blue = roi[y][x][0]
 				B_red = roi[y][x][1]
 				B_blue = roi[y][x][2]
 			else:
-				#print "B out"
 				B_blue = roi[y+1][x][0] & 0xff
 				B_red = roi[y+1][x][1]
 				B_green = roi[y+1][x][2]
 			if (x>=(width1-1) or ((y+1)>=(height1-1))):
-				#print "C in"
 				C_blue = roi[y][x][0]
 				C_red = roi[y][x][1]
 				C_green = roi[y][x][2]
 			else:
-				#print "C  out"
-				#print roi.shape
 				C_blue = roi[y][x+1][0] & 0xff
 				C_red = roi[y][x+1][1]
 				C_green = roi[y][x+1][2]
 			if ((x+1)>=(width1-1) or (y+1)>=(height1-1)):
-				#print "D in"
 				D_blue = roi[y][x][0] & 0xff
 				D_red = roi[y][x][1]
 				D_green = roi[y][x][2]
 			else:
-				#print "D out"
 				D_blue = roi[y+1][x+1][0] & 0xff
 				D_red = roi[y+1][x+1][1]
 				D_green = roi[y+1][x+1][2]
-			#print A
-			#print B
-			#print "D in"
-			#print C
-			#print D
-			#print "D out"
 
-			gray_blue = (int) ( (A_blue * (1 - x_diff) * (1 - y_diff)) + (B_blue * (x_diff) * (1 - y_diff)) + (C_blue * (y_diff)*(1 - x_diff)) + (D_blue * (x_diff*y_diff)))
-			gray_red = (int) ( (A_red * (1 - x_diff) * (1 - y_diff)) + (B_red * (x_diff) * (1 - y_diff)) + (C_red * (y_diff)*(1 - x_diff)) + (D_red * (x_diff*y_diff)))
-			gray_green = (int) ( (A_green * (1 - x_diff) * (1 - y_diff)) + (B_green * (x_diff) * (1 - y_diff)) + (C_green * (y_diff)*(1 - x_diff)) + (D_green * (x_diff*y_diff)))				
+			# Combining all the different channelsn into overall 3 channels
+			newimg_blue = (int) ( (A_blue * (1 - x_diff) * (1 - y_diff)) + (B_blue * (x_diff) * (1 - y_diff)) + (C_blue * (y_diff)*(1 - x_diff)) + (D_blue * (x_diff*y_diff)))
+			newimg_red = (int) ( (A_red * (1 - x_diff) * (1 - y_diff)) + (B_red * (x_diff) * (1 - y_diff)) + (C_red * (y_diff)*(1 - x_diff)) + (D_red * (x_diff*y_diff)))
+			newimg_green = (int) ( (A_green * (1 - x_diff) * (1 - y_diff)) + (B_green * (x_diff) * (1 - y_diff)) + (C_green * (y_diff)*(1 - x_diff)) + (D_green * (x_diff*y_diff)))				
 
-			#print "Gray\n"
-			#print gray
-
+			# Adding the values into the array
 			newrow=count/(width2)
 			newcol=count%(width2)
-	#		print "\nNewrow: "+str(newrow)
-	#		print "\nNewcol: "+str(newcol)
-	#		print img.shape
-	#		print count
-	#		print height2
-	#		print width2
-			finalgray = [gray_blue,gray_red,gray_green]
+			newimg = [newimg_blue,newimg_red,newimg_green]
 			if(newcol == 0):
 				new.append([])
-			new[newrow].append(finalgray)
-			#rew[newrow][newcol]=gray
+			new[newrow].append(newimg)
 			count+=1
 
-	#print new
-	#print roi
 	final_img=np.uint8(new)
-	#print final_img
-	#print img.shape
-	#print final_img.shape
+
+	# Final Image is returned
 	return final_img
 
 if __name__=="__main__":
+
+	# Path of input image is given
 	path = sys.argv[1]
 	img=cv2.imread(path)
 
+	# Pivot co-ordinates and scale are given
 	x_coord = int(sys.argv[2])
 	y_coord = int(sys.argv[3])
 	scale = int(sys.argv[4])
 
+	# Final image is got
 	final_img=zoom(path,x_coord,y_coord,scale)
 	cv2.imshow('Original',img)
 	cv2.imshow('Final',final_img)
-	#newfinal=cv2.cvtColor(final_img,cv2.COLOR_GRAY2BGR)
-	#cv2.imshow('NEW',newfinal)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
